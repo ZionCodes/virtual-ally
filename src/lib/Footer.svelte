b23739c62310401ffbe1ca2b0f8c1985aff14acb
Zion <ziongonet@gmail.com>
2024-09-02 03:28:23 +0100

    removed footer links

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -10,7 +10,7 @@
       <!-- End Col -->
 
       <!-- List -->
-      <ul class="flex flex-wrap items-center">
+      <!-- <ul class="flex flex-wrap items-center">
         <li class="inline-block relative pe-4 text-xs last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-1.5 before:-translate-y-1/2 before:size-[3px] before:rounded-full before:bg-gray-400">
           <a class="text-xs text-gray-500 underline hover:text-gray-800 hover:decoration-2 focus:outline-none focus:decoration-2" href="#">
             X (Twitter)
@@ -26,7 +26,7 @@
             Github
           </a>
         </li>
-      </ul>
+      </ul> -->
       <!-- End List -->
     </div>
   </div>
